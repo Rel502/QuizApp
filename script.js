4664e326b06eb9b69fd51690ba9a9df2f1038ea9
Michael Zeitler <m.zeitler1987@gmail.com>
2024-07-03 16:09:46 +0200

    basic functionality implemented

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let totalQuestionsNumber = questions.length;    /*---> 1 von X Fragen */
 let currentQuestionNumber = 1;                  /*---> X von 10 Fragen */
 
 let currentQuestionIndex = 0;
+let playerScore = 0;
+let quizProgress = 0;
+
+let rightAnswerSound = new Audio ('./assets/audio/success.mp3');
+let wrongAnswerSound = new Audio ('./assets/audio/fail.mp3');
+let endingSound = new Audio ('./assets/audio/ending.mp3');
 
 function renderCurrentQuestion() {
     let content = document.getElementById('cardContent');
@@ -20,18 +26,68 @@ function validateAnswer(selection) {
     let correct_answer = currentQuestion['correct_answer'];
     let selectedQuestionNumber = selection.slice(-1);
 
-    if (selectedQuestionNumber == correct_answer) {
+    if (givenAnswerIsCorrect(selectedQuestionNumber, correct_answer)) {
         document.getElementById(selection).parentNode.classList.add('bg-success-subtle');
+        playerScore++;
+        rightAnswerSound.play();
     } else {
         document.getElementById(selection).parentNode.classList.add('bg-danger-subtle');
         document.getElementById(`answer_${correct_answer}`).parentNode.classList.add('bg-success-subtle');
+        wrongAnswerSound.play();
     }
 
     document.getElementById('nextQuestionBtn').disabled = false;
+    quizProgress += 10;
+}
+
+function givenAnswerIsCorrect(selectedQuestionNumber, correct_answer) {
+    return selectedQuestionNumber == correct_answer;
 }
 
 function nextQuestion() {
+    if (notAllQuestionsAnswered()) {
+        proceedQuiz();
+        renderCurrentQuestion();
+    } else {
+        renderEndScreen();
+    }
+}
+
+function proceedQuiz() {
     currentQuestionIndex++;
     currentQuestionNumber++;
+}
+
+function notAllQuestionsAnswered() {
+    return currentQuestionNumber < totalQuestionsNumber;
+}
+
+function renderEndScreen() {
+    changeBgImg();
+    endingSound.play();
+    let content = document.getElementById('cardContent');
+    content.innerHTML = '';
+    content.innerHTML += returnEndScreenHTML();
+}
+
+function playAgain() {
+    resetPlayerScore();
+    resetCurrentQuestion();
     renderCurrentQuestion();
+    changeBgImg();
+}
+
+function resetPlayerScore() {
+    playerScore = 0;
+    quizProgress = 0;
+}
+
+function resetCurrentQuestion() {
+    currentQuestionIndex = 0;
+    currentQuestionNumber = 1;
+}
+
+function changeBgImg() {
+    document.getElementById('quizImg').classList.toggle('d-none');
+    document.getElementById('endScreenImg').classList.toggle('d-none');
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -23,9 +23,28 @@ function returnCurrentQuestionHTML(currentQuestion) {
             </div>
         </div>
 
+        <!-- Progress Bar -->
+        <div class="progress mb-2" role="progressbar" aria-label="Basic example" aria-valuenow="${quizProgress}" aria-valuemin="0" aria-valuemax="100">
+            <div id="quizProgress" class="progress-bar progress-bar-striped progress-bar-animated" style="width: ${quizProgress}%"></div>
+        </div>
+
         <div class="question-footer mt-3">
             <p class="mb-0"><b>${currentQuestionNumber} </b>von <b>${totalQuestionsNumber}</b> Fragen</p>
             <button onclick="nextQuestion()" id="nextQuestionBtn" href="#" class="btn btn-primary" disabled>Nächste Frage</button>
+        </div>
+    `;
+}
+
+function returnEndScreenHTML() {
+    return /*html*/`
+        <div >
+            <div class="text-center">
+                <h1>Quiz beendet!</h1>
+                <div class="p-1 text-info-emphasis bg-info-subtle border border-info-subtle rounded-3"><b>SCORE: ${playerScore} / ${totalQuestionsNumber}<b></div>
+            </div>
+
+            <button onclick="playAgain()" type="button" class="mt-2 btn btn-outline-info" style="width: 100%">Neuer Versuch</button>
+        </div>
         </div>
     `;
 }
